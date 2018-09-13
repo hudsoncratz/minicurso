3a36286944c51ad2140d8cdfdd705afc5cadbeaa
hudsoncratz <hudsoncratz@gmail.com>
2018-09-13 11:52:29 -0300

    versao final

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const restify = require('restify')
 
 const server = restify.createServer()
 
+const homeRota = require('./rotas/homeRota')
+
+const pessoasRotas = require('./rotas/pessoasRotas')
+
+server.use(restify.plugins.queryParser())
+
+homeRota(server)
+pessoasRotas(server)
+
 const msgServidorOn = () => {
 	console.log('servidor rodando...')
 }
